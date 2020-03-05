943347c1e8446172412f4c7ccfdbc287e38654a9
Benjamin Chacko <benjaminchacko@outlook.com>
2020-03-04 19:03:19 -0700

    updated images

--- src/components/test.js
+++ src/components/test.js
@@ -1,32 +1,32 @@
 const DarkMode = () => {
-    const [darkTheme, setDarkTheme] = useState(getDefaultTheme())
+  const [darkTheme, setDarkTheme] = useState(getDefaultTheme());
 
-    useEffect(() => {
-      localStorage.setItem('dark', JSON.stringify(darkTheme))
-    }, [darkTheme])
+  useEffect(() => {
+    localStorage.setItem("dark", JSON.stringify(darkTheme));
+  }, [darkTheme]);
 
-    function getDefaultTheme(){
-      const selectedTheme = JSON.parse(localStorage.getItem('dark'))
-      return selectedTheme || false
-    }
+  function getDefaultTheme() {
+    const selectedTheme = JSON.parse(localStorage.getItem("dark"));
+    return selectedTheme || false;
+  }
 
-    return (
-      <div className={darkTheme ? 'dark-theme': 'light-theme'}>
-        <nav>
+  return (
+    <div className={darkTheme ? "dark-theme" : "light-theme"}>
+      <nav>
         <div className="button-container">
-				<Toggle
+          <Toggle
             defaultChecked={darkTheme}
             icons={false}
-            onChange={() => setDarkTheme(prevTheme => !prevTheme)}/>
-          </div>
-        </nav>
-        <div className="content">
-          <h1>{darkTheme ? 'Dark Mode' : 'Light Mode'}</h1>
-          <p>
-            Do take a note of the <code>color</code> property in the nav bar.
-          </p>
+            onChange={() => setDarkTheme(prevTheme => !prevTheme)}
+          />
         </div>
+      </nav>
+      <div className="content">
+        <h1>{darkTheme ? "Dark Mode" : "Light Mode"}</h1>
+        <p>
+          Do take a note of the <code>color</code> property in the nav bar.
+        </p>
       </div>
-      )
-  };
-
+    </div>
+  );
+};
